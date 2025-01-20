a6291866ae9db910946eb4ce890cc02c74fc514e
Eric Wong <ericsyw@gmail.com>
2025-01-20 19:48:44 +0800

    Add CLI interface to expose the agent_builder

---
+++ intellifun/cli.py
@@ -0,0 +1,16 @@
+import argparse
+from intellifun.agent_builder import talk_to_agent_builder
+
+def main():
+    parser = argparse.ArgumentParser(description='Intellifun CLI')
+    subparsers = parser.add_subparsers(dest='command')
+    
+    # Add agent_builder subcommand
+    subparsers.add_parser('agent_builder', help='Run the agent builder')
+    
+    args = parser.parse_args()
+    
+    if args.command == 'agent_builder':
+        talk_to_agent_builder()
+    else:
+        parser.print_help()
