77bd53df417ede7aca92f1e445cd1b56a09abe1a
Eric Wong <ericsyw@gmail.com>
2025-02-21 22:50:12 +0800

    Remove Context in module

--- intellifun/__init__.py
+++ intellifun/__init__.py
@@ -1,9 +1,9 @@
 from intellifun.LLM import LLM
-from intellifun.agent import Agent, Tool, Context
+from intellifun.agent import Agent, Tool
 from intellifun.agent_memory import AgentMemoryBank, AgentMemory
 from intellifun.LLMFunc import llmfunc, CheckResult
 from intellifun.backends.openai import GPTModels
 from intellifun.backends.anthropic import AnthropicModels
 
-__all__ = ['LLM', 'Tool', 'Agent', 'Context', 'AgentMemoryBank', 'AgentMemory',
+__all__ = ['LLM', 'Tool', 'Agent', 'AgentMemoryBank', 'AgentMemory',
            'llmfunc', 'CheckResult', 'GPTModels', 'AnthropicModels']
